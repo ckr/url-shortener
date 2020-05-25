fa6cc2df7e29dc18eea0f60853095c39287cbb95
Constantinos Kouloumbris <c@kouloumbris.com>
2020-05-25 23:13:40 +0100

    added lambda

--- lib/ckr-cy-url-shortener-stack.ts
+++ lib/ckr-cy-url-shortener-stack.ts
@@ -6,12 +6,20 @@ import {
     CloudFrontWebDistribution,
     SSLMethod,
     SecurityPolicyProtocol,
-    OriginProtocolPolicy
+    OriginProtocolPolicy,
+    CloudFrontAllowedMethods,
+    CloudFrontAllowedCachedMethods
 } from '@aws-cdk/aws-cloudfront';
 import { ARecord, RecordTarget } from '@aws-cdk/aws-route53';
 import { CloudFrontTarget } from '@aws-cdk/aws-route53-targets';
 import { CertificateAttributes } from './url-shortner-cert-stack';
-import { LambdaRestApi } from '@aws-cdk/aws-apigateway';
+import {
+    RestApi,
+    LambdaRestApi,
+    LambdaIntegration,
+    PassthroughBehavior,
+    IntegrationType
+} from '@aws-cdk/aws-apigateway';
 import {
     Function as LamdaFunction,
     Runtime as LamdaRuntime,
@@ -60,26 +68,48 @@ export class CkrCyUrlShortenerStack extends cdk.Stack {
 
         this.createS3Bucket('Bucket', this.certAttr.zoneName);
 
-        // const shortLamda = new LamdaFunction(this, 'ShortnerLamda', {
-        //     runtime: LamdaRuntime.NODEJS_12_X,
-        //     handler: 'index.handler',
-        //     code: LamdaCode.fromAsset('./lambda'),
-        //     environment: {
-        //         'S3_BUCKET': this.bucket.bucketName,
-        //         'S3_PREFIX': 'u',
-        //         'S3_REGION': Stack.of(this).region
-        //     }
-        // });
+        const shortLamda = new LamdaFunction(this, 'ShortnerLamda', {
+            runtime: LamdaRuntime.NODEJS_12_X,
+            handler: 'index.handler',
+            code: LamdaCode.fromAsset('./lambda'),
+            environment: {
+                'S3_BUCKET': this.bucket.bucketName,
+                'S3_PREFIX': 'u',
+                'S3_REGION': Stack.of(this).region
+            }
+        });
 
-        // const api = new LambdaRestApi(this, 'ShortnerApi', {
-        //     restApiName: 'Lambda Shortener Service',
-        //     description: 'Rest API for URL Shortener',
-        //     handler: shortLamda,
-        //     proxy: false
-        // });
+        this.bucket.grantDelete(shortLamda);
+        this.bucket.grantPut(shortLamda);
+        this.bucket.grantRead(shortLamda);
 
-        // const items = api.root.addResource('prod');
-        // items.addMethod('POST');
+        const api = new RestApi(this, 'ShortnerApi', {
+            restApiName: 'Lambda Shortener Service',
+            description: 'Rest API for URL Shortener',
+            // handler: shortLamda,
+            // proxy: false,
+        });
+
+        const integration = new LambdaIntegration(shortLamda, {
+            proxy: false,
+            passthroughBehavior: PassthroughBehavior.WHEN_NO_MATCH,
+            integrationResponses: [
+                {
+                    statusCode: '200',
+                }
+            ],
+            // requestTemplates: {
+            //     'application/json': JSON.stringify({ 'statusCode': '200' })
+            // }
+        });
+
+        api.root.addMethod('POST', integration, {
+            methodResponses: [
+                {
+                    statusCode: '200'
+                }
+            ]
+        });
 
         const cloudFront = new CloudFrontWebDistribution(this, 'CloudFront', {
             comment: 'CloudFront distribution used as a front end to the server-less URL Shortener',
@@ -109,18 +139,22 @@ export class CkrCyUrlShortenerStack extends cdk.Stack {
                         },
                     ]
                 },
-                // {
-                //     originPath: '/prod',
-                //     customOriginSource: {
-                //         domainName: api.root.
-                //         originProtocolPolicy: OriginProtocolPolicy.MATCH_VIEWER
-                //     },
-                //     behaviors: [
-                //         {
-                //             pathPattern: '/prod/*'
-                //         }
-                //     ]
-                // },
+                {
+                    originPath: '',
+                    customOriginSource: {
+                        //TODO: This is a hack find a way to fix
+                        domainName: `${api.restApiId}.execute-api.${this.region}.${this.urlSuffix}`,
+                        originProtocolPolicy: OriginProtocolPolicy.MATCH_VIEWER,
+                    },
+                    behaviors: [
+                        {
+                            pathPattern: '/prod/*',
+                            allowedMethods: CloudFrontAllowedMethods.ALL,
+                            cachedMethods: CloudFrontAllowedCachedMethods.GET_HEAD_OPTIONS,
+                            
+                        }
+                    ]
+                },
                 {
                     originPath: '/u',
                     customOriginSource: {
